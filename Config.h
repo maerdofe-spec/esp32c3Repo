6f44aca293d432ed3ad66895ab64794643272701
icewalk7 <77discount@sjtu.edu.cn>
2026-06-10 16:16:52 +0800

    Initialize the repo.

---
+++ Config.h
@@ -0,0 +1,29 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// 硬件引脚定义
+#define PIN_SDA 6
+#define PIN_SCL 7
+
+#define PIN_BATTERY_ADC 4
+
+// 任务参数
+#define TARGET_DEPTH_DEEP 2.5f    // 目标深度1: 2.5米
+#define TARGET_DEPTH_SHALLOW 0.4f // 目标深度2: 0.4米
+#define HOVER_DURATION 30000      // 悬停时间: 30秒 (毫秒)
+#define LOG_INTERVAL 1000         // 数据记录间隔: 1秒
+
+// 身份信息
+#define COMPANY_ID "Blue Fins"
+
+// WiFi 参数
+#define WIFI_SSID           "FINS"  //esp32_sjtu
+#define WIFI_PASSWORD       "fins1896"  //esp32_sjtu
+// #define WIFI_SSID           "esp32_sjtu"
+// #define WIFI_PASSWORD       "esp32_sjtu"
+
+// MQTT 参数
+#define MQTT_HOST "192.168.8.86"
+#define MQTT_PORT 1883
+#define MQTT_TOPIC_COUNTER "/counter" // MQTT_TEST
+#define MQTT_TOPIC_DEBUG "/debug"     // DEBUG
---
+++ mate_float_re.ino
@@ -0,0 +1,11 @@
+#include "Config.h"
+
+void setup() {
+  Serial.begin(115200);
+  delay(500); // 等待串口稳定
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
